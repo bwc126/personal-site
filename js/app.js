5fbb89b5f6555b26ae16f6f61dba8dd5ca529d92
Brian Coe <bwc126@gmail.com>
2016-05-09 11:06:22 -0400

    feat: Mouseover changes jumbotron pic to corresponding project pic, but breaks image responsiveness

--- js/app.js
+++ js/app.js
@@ -1,18 +1,34 @@
 (function($) {
   console.log("app starting");
-  // Get each project on the page
-  // Bind an event trigger to each project
-  var spotlight = function(src) {
-    console.log('firing spotlight');
-    $('.jumbotron img').attr('src',src);
+  // // Get each project on the page
+  // // Bind an event trigger to each project
+  // var spotlight = function(src) {
+  //   console.log('firing spotlight');
+  //   $('.jumbotron img').attr('src',src);
+  // };
+  var projects = $(".project img");
+  var numProj = projects.length;
+
+  for (var i = 0; i<(numProj); i++) {
+
+    $(projects[i]).mouseover(function() {
+      var picSrc = $(this).attr('src');
+      console.log(picSrc);
+      (function() {
+        console.log('anon spotlight firing');
+        $('.jumbotron img').attr('srcset',picSrc);
+      }(picSrc));
+    });
+
   };
-  $('.project').each(function() {
-    // Call spotlight to change which project's picture get's the big screen treatment
-    var self = this;
-    var picSrc = $(self).attr('src');
-    console.dir(self);
-    $(self).mouseover(picSrc,spotlight(picSrc));
-  });
+
+  // $('.project').each(function() {
+  //   // Call spotlight to change which project's picture get's the big screen treatment
+  //   var self = this;
+  //   var picSrc = $(self).attr('src');
+  //   console.dir(self);
+  //   self.mouseover(picSrc,spotlight(picSrc));
+  // });
   // Takes in a project and changes the big jumbotron picture to its picture
 
 })(jQuery);

dbf223600fd19947108a2dfd615b795b74cd3678
Brian Coe <bwc126@gmail.com>
2016-05-11 19:13:05 -0400

    refactor: Change styles, add subject buttons

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@
   var projects = $(".project img");
   var numProj = projects.length;
   var picList = [
-    "images/M/proj1-M.png 800w, images/proj1-L.png 1200w",
-    "images/Proj2-M.png 800w, images/Proj2-L.png 1200w",
+    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
+    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
     "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w"
   ];
 

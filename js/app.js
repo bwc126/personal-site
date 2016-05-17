f0852ec52dfef7a15188396ebd3b6af4e29cff43
Brian Coe <bwc126@gmail.com>
2016-05-17 19:16:16 -0400

    refactor: Separate 'model' data from view handling, create render function for project text (WIP)

--- js/app.js
+++ js/app.js
@@ -1,89 +1,60 @@
 (function($) {
   console.log("app starting");
-  // // Get each project on the page
-  // // Bind an event trigger to each project
-  // var spotlight = function(src) {
-  //   console.log('firing spotlight');
-  //   $('.jumbotron img').attr('src',src);
-  // };
   var picSrc;
   var projects;
-  // picList will initially be whatever the default set of projects is, which should be one of the major categories so that they can be reached again later by clicking one of the buttons. It's a simple array of strings formatted to be used as the 'srcset' attribute.
-  var picList = [
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png"
-  ];
-  // prog, engi, and sci are like picList, but contain the image links for each project within their respective areas.
-  var prog = [
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png"
-  ];
-  var engi = [
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png"
-  ];
-  var sci = [
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png",
-    "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
-    "images/S/Hydra2-S.png"
-  ];
+  // data will initially be whatever the default set of projects is, which should be one of the major categories so that they can be reached again later by clicking one of the buttons.
+  var data = prog;
   // The click triggers for the subject buttons will set the project images to the project images for the appropriate subject area, and then call setSpotlightTriggers to make sure they will change the spotlight upon mouseenter.
   $("#programming").click(function() {
     console.log("Programming active");
-    picList = prog;
+    data = prog;
+    renderProjectText;
     renderProjectImages;
     setSpotlightTriggers;
   });
   $("#engineering").click(function() {
     console.log("Engineering Active");
-    picList = engi;
+    data = engi;
+    renderProjectText;
     renderProjectImages;
     setSpotlightTriggers;
   });
   $("#science").click(function() {
     console.log("Science Active");
-    picList = sci;
+    data = sci;
+    renderProjectText;
     renderProjectImages;
     setSpotlightTriggers;
   });
+  // renderProjectText prepares the textual content of each project; this function is called when a button is pressed and a new subject area of content needs to be loaded.
+  function renderProjectText() {
+    projects = $(".project h4");
+    $(projects[0]).text(data.0.project);
+    $(projects[1]).text(data.1.project);
+    $(projects[2]).text(data.2.project);
+  }
   // renderProjectImages changes the project images based on whatever subject area has been made active (e.g., when the user clicks the respective button, currently this is the only time this should be called).
   function renderProjectImages() {
     projects = $(".project img");
-    $(projects[0]).attr('src', picList[1]);
-    $(projects[1]).attr('src', picList[3]);
-    $(projects[2]).attr('src', picList[5]);
+    $(projects[0]).attr('src', data.0.src);
+    $(projects[1]).attr('src', data.1.src);
+    $(projects[2]).attr('src', data.2.src);
   }
   // For loop construction doesn't play nicely with serial event listener setting, so specific code is written for each project so that mouseenter on its image causes the spotlight image to change.
   function setSpotlightTriggers() {
     projects = $(".project img");
     $(projects[0]).mouseenter(function() {
-      $('.jumbotron img').attr('srcset',picList[0]);
+      $('.jumbotron img').attr('srcset',data.0.srcset);
       console.log("Project 0 has spotlight");
     });
     $(projects[1]).mouseenter(function() {
-      $('.jumbotron img').attr('srcset',picList[2]);
+      $('.jumbotron img').attr('srcset',data.1.srcset);
       console.log("Project 1 has spotlight");
     });
     $(projects[2]).mouseenter(function() {
-      $('.jumbotron img').attr('srcset',picList[4]);
+      $('.jumbotron img').attr('srcset',data.2.srcset);
       console.log("Project 2 has spotlight");
     });
   };
   setSpotlightTriggers();
-
 })(jQuery);
---
+++ js/model.js
@@ -0,0 +1,70 @@
+// prog, engi, and sci are the models for each of the subject areas, containing the image links for each project, their names, web-links, and descriptions. This is used to populate the project areas when a subject button is activated.
+var prog = {
+  0: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  },
+  1: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  },
+  2: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  }
+};
+var engi = {
+  0: {
+    project: "Hydra-Delta",
+    link: "https://github.com/bwc126/hydra-delta",
+    src: "images/S/Hydra2-S.png",
+    srcset: "images/M/Hydra2-M.png 800w, images/L/Hydra2-L.png 1200w",
+    desc: "The Hydra-Delta is my personal 3D printer design. It's based on my experience with buiding printers designed by others, specifically the Eventorbot and RichRap, and inspired by other Delta-configuration printers."
+  },
+  1: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  },
+  2: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  }
+};
+var sci = {
+  0: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  },
+  1: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  },
+  2: {
+    project: "",
+    link: "",
+    src: "",
+    srcset: "",
+    desc: ""
+  }
+};

2ea07253233663e0ee9595a31572794705de3f7d
Brian Coe <bwc126@gmail.com>
2016-05-23 23:45:45 -0400

    feat: Spotlight image will crossfade to new one when a project image has a mouseenter event

--- js/app.js
+++ js/app.js
@@ -69,9 +69,16 @@
   // For loop construction doesn't play nicely with serial event listener setting, so specific code is written for each project so that mouseenter on its image causes the spotlight image to change.
   function setSpotlightTriggers() {
     $(".project img").each(function(index) {
+      // Any time a project image is entered, the spotlight img will fadeout, set new src/srcset according to the project that was entered, and then fade back in.
       $(this).mouseenter(function() {
-        $('.jumbotron img').attr('src', data[index].src);
-        $('.jumbotron img').attr('srcset',data[index].srcset);
+        // Callbacks to jQ animation functions will execute after the ani completes, so this will cause the jumbotron image to fadeOut, and then execute the code passed into the anon calllback.
+        $(".jumbotron img").fadeOut(function() {
+          // .load() will make sure the jQ object is ready on the DOM before proceeding with the anon CB passed to it, in this case, fadeIn, ensuring our image is ready before we attempt to fade it back in.
+          $(this).load(function() { $(this).fadeIn(); });
+          // These two .attr calls set a new src/srcset for the spotlight once the fadeout is complete. Once the images are loaded an ready, the fadeIn call above will execute.
+          $(this).attr("src", data[index].src);
+          $(this).attr("srcset", data[index].srcset);
+        });
       });
     });
   };

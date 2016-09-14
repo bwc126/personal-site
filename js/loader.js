a48d4ae7753af783826c28ec8b214a74d76570e9
Brian Coe <bwc126@gmail.com>
2016-09-13 23:35:45 -0400

    fix: Reset script to empty state, as preloading is yet to be implemented

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,6 @@
 "use strict"
 
 (function($) {
-  var image;
-  var sources = [prog,sci,engi];
-  for (var i = 0, i < 3; i++) {
-    for (var j = 0, i < 3; i++) {
-      image = new Image();
-      image.src = sources[i][j].srcset.split(" ")[2];
-    }
-  }
+  console.log("loader.js loaded");
+
 })(jQuery)

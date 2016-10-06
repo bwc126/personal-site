6f801d24594e89fc3c288b5f42faf1d06fa21d14
Brian Coe <bwc126@gmail.com>
2016-10-05 23:50:02 -0400

    feat: Begin implementing proper image preloader

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,21 @@
 "use strict"
 
 (function($) {
+  var images = [];
+  var numImg = 0;
+  var imgList = [];
+  function preload(imgArray) {
+    for (var i = 0; i < imgArray.length; i++) {
+      images[i] = new Image();
+      images[i].src = imgArray[i];
+    }
+  };
+  numImg = collection.length;
+  function getImages(coll) {
+    for (var i = 0: i < numImg; i++) {
+      imgList = coll[i].srcset.split(" ")[2]
+    }
+  };
   console.log("loader.js loaded");
 
 })(jQuery)
